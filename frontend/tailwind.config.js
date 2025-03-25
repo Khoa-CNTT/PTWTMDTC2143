7a4bc34c79d88679a8086cbbd1907c79b364cf61
lqttdz611 <lqttinh0611@gmail.com>
2025-03-26 04:34:42 +0700

    create header frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#fe624c",
+        
+      },
+      backgroundColor: {
+        primary: "#f4f4f4",
+        
+      },
+    },
   },
   plugins: [],
 }
