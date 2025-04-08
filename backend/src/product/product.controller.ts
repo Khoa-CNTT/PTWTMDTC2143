fc26d2c5ce92e43e727e976514eef1b8191d5159
Nguyen Anh Tho <anhthobeo1006@gmail.com>
2025-04-08 11:28:59 +0700

    feat(product): implement update functionality for product and variant

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ProductCreateDTO } from './dto/product-create.dto';
 import { ProductResponseDTO } from './dto/product-response.dto';
 import { ProductService } from './product.service';
 import { VariantResponseDTO } from './dto/variant-response.dto';
 import { VariantCreateDTO } from './dto/variant-create.dto';
+import { ProductUpdateDTO } from './dto/product-update.dto';
+import { VariantUpdateDTO } from './dto/variant-update.dto';
 
 @Controller('product')
 export class ProductController {
@@ -25,5 +34,21 @@ export class ProductController {
       productId,
       variantCreateDTO
     );
+  }
+
+  @Put(':productId')
+  async updateProduct(
+    @Param('productId', ParseUUIDPipe) productId: string,
+    @Body() productUpdateDTO: ProductUpdateDTO
+  ) {
+    return this.productService.updateProduct(productId, productUpdateDTO);
+  }
+
+  @Put('variants/:variantId')
+  async updateVariant(
+    @Param('variantId', ParseUUIDPipe) variantId: string,
+    @Body() variantUpdateDTO: VariantUpdateDTO
+  ) {
+    return this.productService.updateVarriant(variantId, variantUpdateDTO);
   }
 }
