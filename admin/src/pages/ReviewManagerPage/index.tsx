3ca9ddadd1d98fadbe2b0167f1653f5f340ae830
larv412 <larv412@gmail.com>
2025-05-02 11:48:27 +0700

    feat(reviewmanager): add admin interface in admin

---
+++ admin/src/pages/ReviewManagerPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReviewManager from '../../components/ReviewManager';
+
+const ReviewManagerPage: React.FC = () => {
+  return (
+    <div>
+      <ReviewManager />
+    </div>
+  );
+};
+
+export default ReviewManagerPage;
