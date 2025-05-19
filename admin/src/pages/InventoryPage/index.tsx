cab6d858648d2db892768d768d55ea9159607ccc
larv412 <larv412@gmail.com>
2025-05-19 15:07:00 +0700

    feat(inventory): add admin interface in admin

---
+++ admin/src/pages/InventoryPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Inventory from './../../components/Inventory/index';
+
+const InventoryPage: React.FC = () => {
+  return (
+    <div>
+      <Inventory />
+    </div>
+  );
+};
+
+export default InventoryPage;
