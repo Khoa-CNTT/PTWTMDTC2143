2f8d142486b15c66479225902a85ecedc8e90990
anhthodev <anhthobeo1006@gmail.com>
2025-05-24 10:16:27 +0700

    feat(auth): add role-based access control for wishlist endpoints

--- backend/src/auth/config/role-policy.config.ts
+++ backend/src/auth/config/role-policy.config.ts
@@ -3,155 +3,132 @@ import { RoleEnum } from '@prisma/client';
 export const RolePolicy = {
   '/brands': {
     POST: [RoleEnum.ADMIN],
-    GET: [RoleEnum.ADMIN, RoleEnum.USER],
-  },
-  '/brands/:id': {
-    PUT: [RoleEnum.ADMIN, RoleEnum.MANAGER],
+    PUT: [RoleEnum.MANAGER, RoleEnum.ADMIN],
     DELETE: [RoleEnum.ADMIN],
-    GET: [RoleEnum.ADMIN, RoleEnum.USER],
   },
-  '/brands/name/:name': {
-    GET: [RoleEnum.ADMIN, RoleEnum.USER],
-  },
-
-  // Categories
   '/category': {
     POST: [RoleEnum.ADMIN],
-    GET: [RoleEnum.ADMIN, RoleEnum.USER],
-  },
-  '/category/:id': {
-    PUT: [RoleEnum.ADMIN, RoleEnum.MANAGER],
+    PUT: [RoleEnum.MANAGER, RoleEnum.ADMIN],
     DELETE: [RoleEnum.ADMIN],
-    GET: [RoleEnum.ADMIN, RoleEnum.USER],
   },
-  '/category/subcategories/:id': {
-    GET: [RoleEnum.ADMIN, RoleEnum.USER],
-  },
-  '/category/parent/:id': {
-    GET: [RoleEnum.ADMIN, RoleEnum.USER],
-  },
-
-  // Products
-  '/product/create': {
-    POST: [RoleEnum.ADMIN],
-  },
-  '/product/:productId': {
-    PUT: [RoleEnum.ADMIN, RoleEnum.MANAGER],
-  },
-  '/product/:productId/variants': {
-    POST: [RoleEnum.ADMIN],
-  },
-  '/product/:variantId/variants': {
-    PUT: [RoleEnum.ADMIN, RoleEnum.MANAGER],
-  },
-  '/product/by-category': {
-    GET: [RoleEnum.ADMIN, RoleEnum.USER],
-  },
-  '/product/variants/:variantId': {
-    GET: [RoleEnum.ADMIN, RoleEnum.USER],
-  },
-  '/product/search': {
-    GET: [RoleEnum.ADMIN, RoleEnum.USER],
-  },
-
-  // Inventory
-  '/inventory': {
-    POST: [RoleEnum.ADMIN],
-    PUT: [RoleEnum.ADMIN],
-  },
-  '/inventory/check-stock': {
-    POST: [RoleEnum.ADMIN, RoleEnum.USER],
-  },
-  '/inventory/variant/:variantId': {
-    GET: [RoleEnum.ADMIN, RoleEnum.USER],
-  },
-
-  // Reviews
-  '/reviews': {
-    POST: [RoleEnum.USER, RoleEnum.ADMIN],
-  },
-
-  // Carts
   '/cart': {
-    GET: [RoleEnum.USER, RoleEnum.ADMIN],
-    POST: [RoleEnum.USER, RoleEnum.ADMIN],
-    PATCH: [RoleEnum.USER, RoleEnum.ADMIN],
-    DELETE: [RoleEnum.USER, RoleEnum.ADMIN],
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+    POST: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+    PATCH: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+    DELETE: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
   },
   '/cart/item/:id': {
-    DELETE: [RoleEnum.USER, RoleEnum.ADMIN],
-  },
-  '/cart/selected-items': {
-    GET: [RoleEnum.USER, RoleEnum.ADMIN],
-  },
-  '/cart/update-quantity': {
-    PATCH: [RoleEnum.USER, RoleEnum.ADMIN],
-  },
-  '/cart/select-item': {
-    PATCH: [RoleEnum.USER, RoleEnum.ADMIN],
+    DELETE: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
   },
   '/cart/clear': {
-    DELETE: [RoleEnum.USER, RoleEnum.ADMIN],
+    DELETE: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
   },
-
-  // orders
-  '/order': {
-    POST: [RoleEnum.USER, RoleEnum.ADMIN],
-    GET: [RoleEnum.USER, RoleEnum.ADMIN],
-  },
-  '/order/:id': {
-    GET: [RoleEnum.USER, RoleEnum.ADMIN],
-  },
-  '/order/:id/status': {
-    PATCH: [RoleEnum.ADMIN, RoleEnum.MANAGER],
-  },
-
-  // Discount
   '/discounts': {
-    GET: [RoleEnum.ADMIN, RoleEnum.MANAGER],
-    POST: [RoleEnum.ADMIN],
+    POST: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
   },
   '/discounts/:id': {
-    GET: [RoleEnum.ADMIN, RoleEnum.MANAGER],
-    PATCH: [RoleEnum.ADMIN],
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+    PATCH: [RoleEnum.MANAGER, RoleEnum.ADMIN],
     DELETE: [RoleEnum.ADMIN],
   },
-
-  //Voucher
+  '/inventory': {
+    GET: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+    POST: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+    PUT: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/inventory/check-stock': {
+    POST: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/inventory/variant/:variantId': {
+    GET: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/order': {
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+    POST: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN], // VD: tạo order
+  },
+  '/order/admin/all': {
+    GET: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/order/:id': {
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/order/:id/status': {
+    PATCH: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/order/prepare-paypal': {
+    POST: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/order/confirm-paypal': {
+    POST: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/product/create': {
+    POST: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/product/:productId': {
+    PUT: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/product/:productId/variants': {
+    POST: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/product/:variantId/variants': {
+    PUT: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/returns/order/:orderId': {
+    POST: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/returns': {
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/returns/:id': {
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/returns/:id/status': {
+    PATCH: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/reviews': {
+    POST: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+    GET: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/reviews/user/:name/reviews': {
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/reviews/:id': {
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
+  '/reviews/:id/hide': {
+    PATCH: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+  },
   '/voucher': {
-    GET: ['ADMIN', 'MANAGER'],
-    POST: ['ADMIN'],
+    POST: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
   },
   '/voucher/:id': {
-    GET: ['ADMIN', 'MANAGER'],
-    PATCH: ['ADMIN'],
-    DELETE: ['ADMIN'],
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+    PATCH: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+    DELETE: [RoleEnum.ADMIN],
   },
   '/voucher/:id/toggle-status': {
-    PATCH: ['ADMIN'],
+    PATCH: [RoleEnum.MANAGER, RoleEnum.ADMIN],
   },
-
-  // Warehouse
   '/warehouses': {
-    GET: ['ADMIN', 'MANAGER'],
-    POST: ['ADMIN'],
+    POST: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
   },
   '/warehouses/:id': {
-    GET: ['ADMIN', 'MANAGER'],
-    PUT: ['ADMIN'],
-    DELETE: ['ADMIN'],
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+    PUT: [RoleEnum.MANAGER, RoleEnum.ADMIN],
+    DELETE: [RoleEnum.ADMIN],
   },
   '/warehouses/:id/status': {
-    PUT: ['ADMIN'],
+    PUT: [RoleEnum.MANAGER, RoleEnum.ADMIN],
   },
-
-  //wishlist
   '/wishlist': {
-    GET: ['ADMIN', 'USER', 'MANAGER'],
-    POST: ['USER', 'MANAGER'],
+    GET: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
+    POST: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
   },
   '/wishlist/:variantId': {
-    DELETE: ['USER'],
+    DELETE: [RoleEnum.USER, RoleEnum.MANAGER, RoleEnum.ADMIN],
   },
 };
 
@@ -165,4 +142,32 @@ export const PublicRoutes = [
   { path: '/email/verify-otp', method: 'POST' },
   { path: '/email/resend-otp', method: 'POST' },
   { path: '/query', method: 'POST' },
+
+  // Brands
+  { path: '/brands', method: 'GET' },
+  { path: '/brands/:id', method: 'GET' },
+  { path: '/brands/name/:name', method: 'GET' },
+
+  // Category
+  { path: '/category', method: 'GET' },
+  { path: '/category/:id', method: 'GET' },
+  { path: '/category/subcategories/:id', method: 'GET' },
+  { path: '/category/parent/:id', method: 'GET' },
+
+  // Products
+  { path: '/product', method: 'GET' },
+  { path: '/product/by-category', method: 'GET' },
+  { path: '/product/search-by-name', method: 'GET' },
+
+  // Product Variants
+  { path: '/product/variants/:variantId', method: 'GET' },
+  { path: '/product/variants', method: 'GET' },
+
+  { path: '/reviews/product/:productId', method: 'GET' },
+  { path: '/voucher', method: 'GET' },
+  { path: '/voucher/:id', method: 'GET' },
+
+  { path: '/warehouses', method: 'GET' },
+  { path: '/warehouses/:id', method: 'GET' },
+  { path: '/warehouses/name/:name', method: 'GET' },
 ];
--- backend/src/paypal/paypal.controller.ts
+++ backend/src/paypal/paypal.controller.ts
@@ -1,18 +1,7 @@
-// src/paypal/paypal.controller.ts
-import { Controller, Post, Body, Param } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { PaypalService } from './paypal.service';
 
 @Controller('paypal')
 export class PaypalController {
   constructor(private readonly paypalService: PaypalService) {}
-
-  @Post('create-order')
-  async create(@Body('amount') amount: string) {
-    return this.paypalService.createOrder(amount);
-  }
-
-  @Post('capture-order/:orderId')
-  async capture(@Param('orderId') orderId: string) {
-    return this.paypalService.captureOrder(orderId);
-  }
 }
